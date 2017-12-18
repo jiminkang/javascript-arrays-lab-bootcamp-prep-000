69a9f3cf9b2f1c200652ef23023a04c054d79310
Jimin Kang <jimin.kang91@gmail.com>
2017-12-18 03:15:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   return kittens.push(name)
 }
+
+function destructivelyPrependKitthe(name){
+  return kittens.shift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.unshift(name)
+}
+function appendKitten(name){
+  
+}
+function prependKitten(name){
+  
+}
+function removeLastKitten(name){
+  
+}
+function removeFirstKitten(name){
+  
+}
